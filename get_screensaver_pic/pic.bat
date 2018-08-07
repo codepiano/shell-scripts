75fc7ea5898ff32c5e61daa1b060d91f77317bf3
codepiano <codepiano.li@gmail.com>
2018-08-07 21:47:44 +0800

    Update pic.bat

--- get_screensaver_pic/pic.bat
+++ get_screensaver_pic/pic.bat
@@ -1,15 +1,16 @@
 @echo off
-set tmp=%~dp0\screensaver
-set base=(dir /b %localappdata%\Packages\Microsoft.Windows.ContentDeliveryManager_*)
-set res=%res%\LocalState\Assets
+setlocal enabledelayedexpansion
+set tmp=%~dp0\tmp
 echo Store image Temporary directory %tmp%
 
 for %%i in ( %tmp% ) do  if not exist %%i md %%i
 
-for /f "delims=" %%b in ('dir /a-d /b /s %res%') do (   
-    if not exist %tmp%/%%~nb.jpg if %%~zb GTR 409600 xcopy %%b %tmp%
+for /f  %%i in ('dir /b %localappdata%\Packages\Microsoft.Windows.ContentDeliveryManager_*') do (
+    set res=%localappdata%\Packages\%%i\LocalState\Assets
+    for /f "delims=" %%b in ('dir /a-d /b /s !res!') do (   
+       if not exist %tmp%/%%~nb.jpg if %%~zb GTR 409600 xcopy %%b %tmp%
+    )
 )
-
 ren %tmp%\* *.jpg
 echo Select Picture
 start /w %tmp%
