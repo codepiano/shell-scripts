e2b190c99046d33e0503b5e95b9b93a6c7cf8ef4
codepiano <codepiano.li@gmail.com>
2018-08-10 17:40:10 +0800

    remove trailing space

--- get_screensaver_pic/pic.bat
+++ get_screensaver_pic/pic.bat
@@ -7,7 +7,7 @@ for %%i in ( %tmp% ) do  if not exist %%i md %%i
 
 for /f  %%i in ('dir /b %localappdata%\Packages\Microsoft.Windows.ContentDeliveryManager_*') do (
     set res=%localappdata%\Packages\%%i\LocalState\Assets
-    for /f "delims=" %%b in ('dir /a-d /b /s !res!') do (   
+    for /f "delims=" %%b in ('dir /a-d /b /s !res!') do (
        if not exist %tmp%/%%~nb.jpg if %%~zb GTR 409600 xcopy %%b %tmp%
     )
 )
